6aca05b5e78c187b53612d2c5c8e44c8f66aa3ed
Heath Blackmon <coleoguy@gmail.com>
2024-06-25 11:22:04 -0500

    add pseudocode script

---
+++ script.R
@@ -0,0 +1,21 @@
+# read in a FASTA file with the genome of the 
+# organism you are studying you will do this with the R package
+# APE which has a function designed to read FASTA files
+
+# read in the GFF file that contains the annotation of the genome
+# you should be able to use the function read.table to read the 
+# GFF file
+
+# Process the GFF file
+## find genes with multiple exons
+## extract out the coding version of the gene with no exons
+## save the intronless version of each gene in a FASTA File
+
+# Find retrogenes
+## use blast to search for retrogenes
+## if a retrogene matches your requirements then record
+### parent gene name
+### parent gene location (chromosome and location)
+### daughter gene location
+
+
